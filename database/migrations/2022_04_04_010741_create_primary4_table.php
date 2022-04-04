cd2e481ecb41b0599f8d2646505e0beb5b40ceba
Reyoe <51037216+Reyoe@users.noreply.github.com>
2022-04-03 20:16:18 -0500

    Se generan migraciones de las tablas de los grados restantes

---
+++ database/migrations/2022_04_04_010741_create_primary4_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrimary4Table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('primaryFourth', function (Blueprint $table) {
+            $table->id();
+            $table->string('question');
+            $table->string('type');
+            $table->string('firstAnswer');
+            $table->string('secondAnswer');
+            $table->string('thirdAnswer');
+            $table->string('quarterAnswer');
+            $table->string('correctAnswer');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('primaryFourth');
+    }
+}
