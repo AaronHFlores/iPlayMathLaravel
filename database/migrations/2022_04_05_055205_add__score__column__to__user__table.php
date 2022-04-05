0dc4eab60eac28b6f572484248e51b30e8f7ef13
Reyoe <51037216+Reyoe@users.noreply.github.com>
2022-04-05 05:08:42 -0500

    Valor por default al score

--- database/migrations/2022_04_05_055205_add__score__column__to__user__table.php
+++ database/migrations/2022_04_05_055205_add__score__column__to__user__table.php
@@ -14,7 +14,7 @@ class AddScoreColumnToUserTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('score');
+            $table->integer('score')->default(0);
         });
     }
 
