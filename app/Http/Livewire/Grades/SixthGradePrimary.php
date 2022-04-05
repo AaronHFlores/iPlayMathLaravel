fac14fd52836e16e313dd84f433d2d73de44f89c
AaronHFlores <aaron.hflores@alumnos.udg.mx>
2022-04-05 01:09:01 -0600

    Cambios a pull random de preguntas

--- app/Http/Livewire/Grades/SixthGradePrimary.php
+++ app/Http/Livewire/Grades/SixthGradePrimary.php
@@ -8,7 +8,7 @@ class SixthGradePrimary extends Component
 {
     public function render()
     {
-        $randomNumber = random_int(1, 22);
+        $randomNumber = random_int(1, 21);
         $exercise = Exercise6::where('id', $randomNumber)->first();
         if($exercise->type==2){
             return view('livewire.exercise.four-option',['primarySixth' => $exercise]);
--- app/Http/Livewire/Grades/ThirdGradeElementary.php
+++ app/Http/Livewire/Grades/ThirdGradeElementary.php
@@ -8,7 +8,7 @@ class ThirdGradeElementary extends Component
 {
     public function render()
     {
-        $randomNumber = random_int(1, 22);
+        $randomNumber = random_int(1, 21);
         $exercise = Exercise3S::where('id', $randomNumber)->first();
         if($exercise->type==2){
             return view('livewire.exercise.four-option',['primarySixth' => $exercise]);
