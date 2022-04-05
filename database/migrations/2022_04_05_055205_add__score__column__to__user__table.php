a19bb0255416f9774d94d6908655227bd148cca9
Reyoe <51037216+Reyoe@users.noreply.github.com>
2022-04-05 00:55:31 -0500

    Se agrega la columna score al user y se corrige down de la migracion de edad

--- database/migrations/2022_04_05_055205_add__score__column__to__user__table.php
+++ database/migrations/2022_04_05_055205_add__score__column__to__user__table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddAge extends Migration
+class AddScoreColumnToUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddAge extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('age');
+            $table->integer('score');
         });
     }
 
@@ -26,7 +26,7 @@ class AddAge extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('age');
+            $table->dropColumn('score');
         });
     }
 }
