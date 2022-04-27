af3e40c3957af5850a61c2c4073a2615f921b883
AaronHFlores <aaron.hflores@alumnos.udg.mx>
2022-04-27 08:52:12 -0600

    Conexion por medio de websockets

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,8 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+                'host' => '12.0.0.1',
+                'port' => 6001,
             ],
         ],
 
