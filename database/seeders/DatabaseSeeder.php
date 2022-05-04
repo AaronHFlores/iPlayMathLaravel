b3371569afc0f6eb33f970d3f7d5971a4eab8bc7
AaronHFlores <aaron.hflores@alumnos.udg.mx>
2022-05-04 05:32:49 -0500

    cambios a seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,15 +16,16 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             UserSeeder::class,
-            primary1Seeder::class,
-            primary2Seeder::class,
-            primary3Seeder::class,
-            primary4Seeder::class,
-            primary5Seeder::class,
-            primary6Seeder::class,
+            Primary1Seeder::class,
+            Primary2Seeder::class,
+            Primary3Seeder::class,
+            Primary4Seeder::class,
+            Primary5Seeder::class,
+            Primary6Seeder::class,
             Elementary1Seeder::class,
             Elementary2Seeder::class,
-            Elementary3Seeder::class
+            Elementary3Seeder::class,
+            RoomSeeder::class
         ]);
     }
 }
--- database/seeders/primary6Seeder.php
+++ database/seeders/primary6Seeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class primary6Seeder extends Seeder
+class Primary6Seeder extends Seeder
 {
     /**
      * Run the database seeds.
