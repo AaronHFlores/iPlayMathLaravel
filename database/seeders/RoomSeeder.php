16ac73b64cc9a73b45a1d0766e80006716efd7da
AaronHFlores <aaron.hflores@alumnos.udg.mx>
2022-05-03 22:37:07 -0500

    seeders room

--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -15,7 +15,304 @@ class RoomSeeder extends Seeder
     public function run()
     {
         DB::table('room')->insert([
-            'name' => 'room4',
+            'room' => 'room1',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room2',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room3',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room4',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room5',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room6',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room7',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room8',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room9',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room10',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room11',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room12',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room13',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room14',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room15',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room16',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room17',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room18',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room19',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room20',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room21',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room22',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room23',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room24',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room25',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room26',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room27',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room28',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room29',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room30',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room31',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room32',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room33',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room34',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room35',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room36',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room37',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room38',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room39',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room40',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room41',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room42',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room43',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room44',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room45',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room46',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room47',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room48',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room49',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room50',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room51',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room52',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room53',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room54',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room55',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room56',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room57',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room58',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room59',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room60',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room61',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room62',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room63',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room64',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room65',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room66',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room67',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room68',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room69',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room70',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room71',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room72',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room73',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room74',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room75',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room76',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room77',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room78',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room79',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room80',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room81',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room82',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room83',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room84',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room85',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room86',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room87',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room88',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room89',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room90',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room91',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room92',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room93',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room94',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room95',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room96',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room97',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room98',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room99',
+        ]);
+        DB::table('room')->insert([
+            'room' => 'room100',
         ]);
     }
 }
